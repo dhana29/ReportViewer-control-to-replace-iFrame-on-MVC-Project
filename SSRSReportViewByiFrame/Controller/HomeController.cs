7d28e61fdf1d35b0dc6b8cf91e50e9b912822f80
Dhana <mcsesas@gmail.com>
2018-08-19 18:42:20 -0400

    Add files via upload

--- SSRSReportViewByiFrame/Controller/HomeController.cs
+++ SSRSReportViewByiFrame/Controller/HomeController.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
+namespace SSRSReportViewByiFrame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["reportURL"] = Uri.EscapeUriString("../.." + System.Configuration.ConfigurationManager.AppSettings.Get("applicationURL") + System.Configuration.ConfigurationManager.AppSettings.Get("ReportViewerURL"));
+            return View();
+        }
+    }
+}

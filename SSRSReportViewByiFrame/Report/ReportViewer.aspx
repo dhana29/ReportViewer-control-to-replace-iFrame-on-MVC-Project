6fb03ef87cf3478e8e5c5b355c94f0d5f880cf5f
Dhana <mcsesas@gmail.com>
2018-08-19 18:46:57 -0400

    Add files via upload

--- SSRSReportViewByiFrame/Report/ReportViewer.aspx
+++ SSRSReportViewByiFrame/Report/ReportViewer.aspx
@@ -1,1 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReportViewer.aspx.cs" Inherits="SSRSReportViewByiFrame.Report.ReportViewer" %>
 
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+                
+            </asp:ScriptManager>
+            <rsweb:ReportViewer id="rvSiteMapping" runat ="server" ShowPrintButton="false"  Width="99.9%" Height="100%" AsyncRendering="true" ZoomMode="Percent" 
+                KeepSessionAlive="true" SizeToReportContent="false" ></rsweb:ReportViewer>  
+    </div>
+    </form>
+</body>
+</html>
---
+++ SSRSReportViewByiFrame/Report/ReportViewer.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+namespace SSRSReportViewByiFrame.Report
+{
+    public partial class ReportViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    string reportFolder = "reportFolder";
+
+                    ReportingServiceViewModel model = new ReportingServiceViewModel();
+                    List<ReportParameter> paramCollection = new List<ReportParameter>();
+
+                    rvSiteMapping.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Remote;
+                    rvSiteMapping.ServerReport.ReportServerUrl = new Uri(System.Configuration.ConfigurationManager.AppSettings["ReportServerUrl"].ToUpper()); // Add the Reporting Server URL  
+                    rvSiteMapping.ServerReport.ReportPath = String.Format("{0}{1}", reportFolder, Request["ReportName"].ToString());
+                    rvSiteMapping.ServerReport.ReportServerCredentials = model.ServerCredentials;
+                    rvSiteMapping.ServerReport.SetParameters(paramCollection.ToArray());
+                    rvSiteMapping.ShowParameterPrompts = false;
+                    rvSiteMapping.SizeToReportContent = true;
+                    rvSiteMapping.ShowPrintButton = false;
+                    rvSiteMapping.ShowDocumentMapButton = true;
+                    rvSiteMapping.Width = Unit.Percentage(100);
+                    rvSiteMapping.Height = Unit.Percentage(100);
+                    rvSiteMapping.ShowBackButton = false;
+                    rvSiteMapping.ServerReport.Refresh();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}

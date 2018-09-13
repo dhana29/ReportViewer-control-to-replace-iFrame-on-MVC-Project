84947317550b6b8744a02ed16ec7e68cb9291473
Dhana <mcsesas@gmail.com>
2018-09-12 23:41:23 -0400

    Add files via upload

---
+++ SSRSReportViewByiFrame/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+namespace SSRSReportViewByiFrame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

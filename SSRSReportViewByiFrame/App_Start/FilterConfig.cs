19a0a017f3323e571cf753fc1bcb0bceb9a450d3
Dhana <mcsesas@gmail.com>
2018-09-12 23:24:36 -0400

    Add files via upload

--- SSRSReportViewByiFrame/App_Start/FilterConfig.cs
+++ SSRSReportViewByiFrame/App_Start/FilterConfig.cs
@@ -1,1 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
 
+namespace SSRSReportViewByiFrame
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
